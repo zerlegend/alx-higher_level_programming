2a9ef3838f3a9326cfe17626738ad044ffc0fb77
zerlegend <zerlegend@gmail.com>
2023-04-03 19:57:24 -0700

    9. A square is a rectangle

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Write a class Rectangle that defines a rectangle by: (based on 8-rectangle.py)"""
+"""Write a class Rectangle that defines a rectangle """
 
 
 class Rectangle:
